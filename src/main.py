c982b6cafdfadd1490d59e921fae192e452b1271
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-08-09 19:40:53 +0900

    sleep エンドポイントを追加

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 import flask
+import time
+import logging
+import sys
 
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 app = flask.Flask('app')
 
@@ -7,6 +12,15 @@ app = flask.Flask('app')
 @app.route('/')
 def root():
     return 'Hello, world'
+
+
+@app.route('/sleep')
+def sleep():
+    for i in range(0, 10):
+        time.sleep(1)
+        logger.debug(f'sleep {i}')
+
+    return 'sleep and wakeup.'
 
 
 if __name__ == '__main__':
