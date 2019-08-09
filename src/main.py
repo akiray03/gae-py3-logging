05f0683341b5ff33baa778760c17f02f4155ed2e
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-08-09 20:00:41 +0900

    Add logging

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import flask
 import time
 import logging
 import sys
+import os
+import json
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -16,8 +18,36 @@ def root():
 
 @app.route('/sleep')
 def sleep():
+    trace_id = flask.request.headers.get('X-Cloud-Trace-Context')
+
     for i in range(0, 10):
         time.sleep(1)
+        # Output:
+        # {
+        #     "Message": "Hi!",
+        #     "severity": "DEFAULT",
+        #     "time": "2019-05-18T13:47:00Z",
+        #     "logging.googleapis.com/trace": "projects/foobar/traces/65ed3bb1ceb342ba0ca62fa64076c738",
+        #     "logging.googleapis.com/spanId": "2325d572b51a4ba6",
+        #     "logging.googleapis.com/sourceLocation": {
+        #         "file": "/tmp/123456/sdlog/buildlog/example_test.go",
+        #         "line": "55",
+        #         "function": "github.com/vvakame/sdlog/buildlog_test.Example_emitJSONPayloadWithEmbed"
+        #     }
+        # }
+
+        j = {
+            'resource': {
+                'labels': {
+                    'module_id': os.environ.get('GAE_SERVICE'),
+                    'project_id': os.environ.get('GAE_APPLICATION'),
+                    'version_id': os.environ.get('GAE_VERSION'),
+                },
+                'type': 'gae_app'
+            },
+            'trace': trace_id,
+        }
+        print(json.dumps(j))
         logger.debug(f'sleep {i}')
 
     return 'sleep and wakeup.'
